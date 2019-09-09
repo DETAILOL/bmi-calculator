c801ff49e7c314d7233267ea8eca194dc181a7e0
user <user@user-PC>
2019-09-09 18:52:53 +0800

    update

--- 106306069_2/src/BMICalculatorTester.java
+++ 106306069_2/src/BMICalculatorTester.java
@@ -1,14 +1,26 @@
+import java.util.Scanner;
  
 public class BMICalculatorTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		boolean doloop = true;
+		while(doloop) {
+			System.out.println("Please enter your weight and height: ");
+			double weight = in.nextDouble();
+			double height = in.nextDouble();
+			BMICalculator bmiCalculator = new BMICalculator(weight, height);
+			bmiCalculator.calculateBMI();
+			System.out.println("The weight input: "+ bmiCalculator.getWeight() + " kg.");
+			System.out.println("The height input: "+ bmiCalculator.getHeight() + " cm.");
+			System.out.println(bmiCalculator.getSensibleBMIMessage());
+			
+			}in.close();
+		}
+		
+		
 	
-		BMICalculator bmiCalculator = new BMICalculator(75.5,175.5);
-		bmiCalculator.calculateBMI();
-		System.out.println("The height input: "+ bmiCalculator.getHeight() + " cm.");
-		System.out.println("The weight input: "+ bmiCalculator.getWeight() + " kg.");
-		System.out.println(bmiCalculator.getSensibleBMIMessage());
-	}
 
 }
